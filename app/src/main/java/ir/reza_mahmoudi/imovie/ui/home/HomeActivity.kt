66dd1ed0d55f52d7c085041cef9aad1481e449bb
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2022-07-24 23:23:58 +0430

    =Create Animated Splash Screen with Lottie

---
+++ app/src/main/java/ir/reza_mahmoudi/imovie/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package ir.reza_mahmoudi.imovie.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.reza_mahmoudi.imovie.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/ir/reza_mahmoudi/imovie/ui/splash/SplashScreen.kt
@@ -0,0 +1,30 @@
+package ir.reza_mahmoudi.imovie.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import ir.reza_mahmoudi.imovie.databinding.ActivitySplashScreenBinding
+import ir.reza_mahmoudi.imovie.ui.home.HomeActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        normalLaunch()
+    }
+    private fun normalLaunch() {
+        binding.loadingAnimation.setAnimation("loading_animation.json")
+        binding.loadingAnimation.playAnimation()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreen, HomeActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
