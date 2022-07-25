f43a2e87a85a7966c0586158a93b1d5664967424
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2022-07-25 10:06:46 +0430

    =Setup MoviesListAdapter

--- app/src/main/java/ir/reza_mahmoudi/imovie/ui/home/HomeActivity.kt
+++ app/src/main/java/ir/reza_mahmoudi/imovie/ui/home/HomeActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import ir.reza_mahmoudi.imovie.R
 import ir.reza_mahmoudi.imovie.data.model.MovieItem
 import ir.reza_mahmoudi.imovie.data.model.SearchResponse
@@ -12,20 +13,35 @@ import ir.reza_mahmoudi.imovie.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
     lateinit var viewModel: HomeViewModel
     private lateinit var binding: ActivityHomeBinding
+    private val moviesAdapter=MoviesListAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel= ViewModelProvider(this)[HomeViewModel::class.java]
-        viewModel.searchMovies("Batman")
-//        binding.swipeRefreshLayout.setOnRefreshListener {
-//            binding.swipeRefreshLayout.isRefreshing=false
-//            viewModel.refresh()
-//        }
+        initViews()
         playLoadingAnimation()
         observeViewModel()
+    }
+    private fun initViews(){
+        viewModel= ViewModelProvider(this)[HomeViewModel::class.java]
+        viewModel.searchMovies("Batman")
+
+        binding.moviesList.apply {
+            layoutManager= LinearLayoutManager(context,
+                LinearLayoutManager.VERTICAL, false)
+            adapter=moviesAdapter
+        }
+
+        binding.searchButton.setOnClickListener {
+            viewModel.searchMovies(binding.searchText.text.toString())
+        }
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.swipeRefreshLayout.isRefreshing=false
+            viewModel.searchMovies(binding.searchText.text.toString())
+        }
     }
     private fun playLoadingAnimation(){
         binding.loadingAnimation.setAnimation("loading_animation.json")
@@ -35,6 +51,7 @@ class HomeActivity : AppCompatActivity() {
         viewModel.movies.observe(this) { movies: List<MovieItem>? ->
             movies?.let {
                 binding.moviesList.visibility = View.VISIBLE
+                moviesAdapter.updateMovies(it)
             }
         }
         viewModel.moviesLoadError.observe(this) { isError: Boolean? ->
---
+++ app/src/main/java/ir/reza_mahmoudi/imovie/ui/home/MoviesListAdapter.kt
@@ -0,0 +1,41 @@
+package ir.reza_mahmoudi.imovie.ui.home
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ir.reza_mahmoudi.imovie.data.model.MovieItem
+import ir.reza_mahmoudi.imovie.databinding.ItemMovieBinding
+import ir.reza_mahmoudi.imovie.utils.getProgressDrawable
+import ir.reza_mahmoudi.imovie.utils.loadImage
+
+class MoviesListAdapter (private var movies: ArrayList<MovieItem>) : RecyclerView.Adapter<MoviesListAdapter.ViewHolder>(){
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateMovies(newMovies:List<MovieItem>){
+        movies.clear()
+        movies.addAll(newMovies)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    override fun getItemCount(): Int =movies.size
+
+    class ViewHolder(private val binding: ItemMovieBinding): RecyclerView.ViewHolder(binding.root){
+        private val progressDrawable= getProgressDrawable(binding.root.context)
+        fun bind(movie: MovieItem){
+            binding.title.text=movie.title
+            binding.type.text=movie.type
+            binding.year.text=movie.year
+            binding.coverImage.loadImage(movie.poster,progressDrawable)
+        }
+    }
+}
